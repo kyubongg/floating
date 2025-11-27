bcd1f05702ff7c7c21c38e9455f35d058016cff0
rbqhd9709 <rbqhd9709@naver.com>
2025-11-27 11:27:39 +0900

    feat: front, back 폴더 생성

---
+++ front/vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    
+    <h2>App.vue</h2>
+    <hr>
+    <MyComponent/>
+    
+  </div>
+</template>
+
+
+<script setup>
+  import MyComponent from './components/MyComponent.vue';
+</script>
+
+
+<style scoped></style>
---
+++ front/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div>
+    
+    <h3>MyComponent</h3>
+    <MyComponentItem/>
+    <MyComponentItem/>
+    
+  </div>
+</template>
+
+
+
+<script setup>
+    // import MyComponentItem from './MyComponentItem.vue';
+    import MyComponentItem from '@/components/MyComponentItem.vue'
+</script>
+
+
+<style scoped></style>
