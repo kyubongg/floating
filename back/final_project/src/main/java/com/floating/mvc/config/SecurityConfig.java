dea5429b46ab349e805203400b8820b2b820b2da
kyubongg <kyubongbong@gmail.com>
2025-12-09 13:28:09 +0900

    feat: security config 수정

--- back/final_project/src/main/java/com/floating/mvc/config/SecurityConfig.java
+++ back/final_project/src/main/java/com/floating/mvc/config/SecurityConfig.java
@@ -4,40 +4,56 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.floating.mvc.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .formLogin(form -> form.disable())  // 기본 로그인 폼 비활성화
-            .httpBasic(basic -> basic.disable());  // Basic 인증 비활성화
-        
-        return http.build();
-    }
-    
-    
-      //jwt
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http
 //            .csrf(csrf -> csrf.disable())
-//            .authorizeHttpRequests(auth -> auth
-//                .requestMatchers("/api/v1/auth/**").permitAll()  // 로그인/회원가입 허용
-//                .requestMatchers("/api/v1/public/**").permitAll()  // 공개 API 허용
-//                .anyRequest().authenticated()  // 나머지는 인증 필요
-//            );
+//            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//            .formLogin(form -> form.disable())  // 기본 로그인 폼 비활성화
+//            .httpBasic(basic -> basic.disable());  // Basic 인증 비활성화
 //        
 //        return http.build();
 //    }
+    
+    
+    //jwt
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session
+            		.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(form -> form.disable())
+            .httpBasic(basic -> basic.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/user/signIn", "/api/v1/user/signUp").permitAll()  // 로그인/회원가입 허용
+                .anyRequest().authenticated()  // 나머지는 인증 필요
+            ).addFilterBefore(
+            		jwtAuthenticationFilter, 
+            		UsernamePasswordAuthenticationFilter.class
+            );
+        
+        return http.build();
+    }
 }
